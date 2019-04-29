be44bd239ff37ede010b65046da93ba40c413442
Atsamaz <atsabitsoev>
2019-04-29 14:39:29 +0300

    commit

---
+++ WaveGradientView/WaveGradientView.swift
@@ -0,0 +1,47 @@
+//
+//  WaveGradientView.swift
+//  WaveGradientView
+//
+//  Created by Ацамаз Бицоев on 29/04/2019.
+//  Copyright © 2019 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class WaveGradientView: UIView {
+    
+    
+    @IBInspectable var color2: UIColor = #colorLiteral(red: 0.368627451, green: 0.2078431373, blue: 0.6941176471, alpha: 1)
+    @IBInspectable var color1: UIColor = #colorLiteral(red: 0.3382141292, green: 0.7531377077, blue: 0.9901052117, alpha: 1)
+    
+
+    override func draw(_ rect: CGRect) {
+       
+        drawWave()
+        
+    }
+    
+    
+    private func drawWave() {
+        
+        let mainPath = UIBezierPath()
+        
+        let startPoint = CGPoint(x: 0, y: bounds.height)
+        let endPoint = CGPoint(x: bounds.width, y: 0.5 * bounds.height)
+        
+        mainPath.move(to: CGPoint(x: bounds.width, y: 0))
+        mainPath.addLine(to: CGPoint(x: 0, y: 0))
+        mainPath.addLine(to: startPoint)
+        mainPath.addCurve(to: endPoint,
+                          controlPoint1: CGPoint(x: startPoint.x + 90,
+                                                 y: (startPoint.y + endPoint.y) / 2 - 100),
+                          controlPoint2: CGPoint(x: endPoint.x - 30,
+                                                 y: (startPoint.y + endPoint.y) / 2))
+        mainPath.close()
+        color1.setFill()
+        mainPath.fill()
+    }
+ 
+
+}

1675c236f17567fc838a1c439ea70556c577e35f
Atsamaz <atsabitsoev>
2019-04-29 15:10:51 +0300

    commit

--- WaveGradientView/WaveGradientView.swift
+++ WaveGradientView/WaveGradientView.swift
@@ -12,35 +12,50 @@ import UIKit
 class WaveGradientView: UIView {
     
     
-    @IBInspectable var color2: UIColor = #colorLiteral(red: 0.368627451, green: 0.2078431373, blue: 0.6941176471, alpha: 1)
-    @IBInspectable var color1: UIColor = #colorLiteral(red: 0.3382141292, green: 0.7531377077, blue: 0.9901052117, alpha: 1)
+    @IBInspectable var color1: UIColor = #colorLiteral(red: 0.368627451, green: 0.2078431373, blue: 0.6941176471, alpha: 1)
+    @IBInspectable var color2: UIColor = #colorLiteral(red: 0.003921568627, green: 0.6745098039, blue: 0.7568627451, alpha: 1)
     
 
     override func draw(_ rect: CGRect) {
        
-        drawWave()
-        
+        drawWaveView()
     }
     
     
-    private func drawWave() {
+    private func drawWaveView() {
         
         let mainPath = UIBezierPath()
         
         let startPoint = CGPoint(x: 0, y: bounds.height)
-        let endPoint = CGPoint(x: bounds.width, y: 0.5 * bounds.height)
+        let endPoint = CGPoint(x: bounds.width, y: 0.52 * bounds.height)
         
         mainPath.move(to: CGPoint(x: bounds.width, y: 0))
         mainPath.addLine(to: CGPoint(x: 0, y: 0))
         mainPath.addLine(to: startPoint)
         mainPath.addCurve(to: endPoint,
-                          controlPoint1: CGPoint(x: startPoint.x + 100,
+                          controlPoint1: CGPoint(x: startPoint.x + 0.2 * bounds.width,
                                                  y: (startPoint.y + endPoint.y) / 2 - 80),
-                          controlPoint2: CGPoint(x: endPoint.x - 50,
+                          controlPoint2: CGPoint(x: endPoint.x - 0.135 * bounds.width,
                                                  y: (startPoint.y + endPoint.y) / 2 + 30))
         mainPath.close()
-        color1.setFill()
-        mainPath.fill()
+        
+        addGradient(to: mainPath)
+    }
+    
+    private func addGradient(to path: UIBezierPath) {
+        
+        let gradient = CAGradientLayer()
+        gradient.colors = [color1.cgColor,
+                           color2.cgColor]
+        gradient.frame = path.bounds
+        gradient.startPoint = CGPoint(x: 0, y: 1)
+        gradient.endPoint = CGPoint(x: 1, y: 0)
+        
+        let shapeMask = CAShapeLayer()
+        shapeMask.path = path.cgPath
+        gradient.mask = shapeMask
+        
+        layer.addSublayer(gradient)
     }
  
 

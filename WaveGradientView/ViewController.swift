bad729f92f52ac681a3f1fc1406648ba714d95e7
Atsamaz <atsabitsoev>
2019-04-29 14:06:24 +0300

    Initial Commit

---
+++ WaveGradientView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaveGradientView
+//
+//  Created by Ацамаз Бицоев on 29/04/2019.
+//  Copyright © 2019 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
